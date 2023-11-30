966989a2bbcf7aedadb10a21db0ab2a1d93015b5
khaled-morgan7 <145543723+khaled-morgan7@users.noreply.github.com>
2023-11-30 09:00:56 +0200

    Update mourse.py

--- mourse.py
+++ mourse.py
@@ -18,3 +18,40 @@ def translate_to_morse(text):
         else:
             return f"Error: '{char}' is not in the dictionary."
     return morse_code
+
+def translate_to_english(morse_code):
+    """Translates Morse code to English text."""
+    english_text = ''
+    morse_to_eng = {v: k for k, v in morse_code_dict.items()}
+    morse_words = morse_code.split()
+    for word in morse_words:
+        if word in morse_to_eng:
+            english_text += morse_to_eng[word]
+        else:
+            return f"Error: '{word}' is not in the dictionary."
+    return english_text
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. convert English to Morse code")
+        print("2. convert Morse code to English")
+        print("3. Main menu")
+
+        choice = input("Enter your choice (1/2/3): ")
+        if choice == '1':
+            text_to_translate = input("Enter the text to translate to Morse code: ")
+            result = translate_to_morse(text_to_translate)
+            print("Translated to Morse code:", result)
+        elif choice == '2':
+            morse_to_translate = input("Enter the Morse code to translate to English: ")
+            result = translate_to_english(morse_to_translate)
+            print("Translated to English:", result)
+        elif choice == '3':
+            print("Thank you for using morse code. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option (1/2/3).")
+
+if __name__ == "__main__":
+    main()
